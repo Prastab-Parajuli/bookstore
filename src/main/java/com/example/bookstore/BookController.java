543ef762b2ddfe3ecf9eac664b2ff66435516a10
Prastab-Parajuli <prastab.parajuli@myy.haaga-helia.fi>
2026-09-17 00:56:58 +0300

    Task 3 create and delete functionality

--- src/main/java/com/example/bookstore/BookController.java
+++ src/main/java/com/example/bookstore/BookController.java
@@ -3,6 +3,8 @@ package com.example.bookstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -22,5 +24,26 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+
+    @PostMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+   
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
